956a3011c7970f82a19e513484e0e0e64067090a
Yosa Rama Dinata <yosamelody07@gmail.com>
2022-01-24 11:29:56 +0800

    ADD: Sample Testing for Sign Up

--- cypress/integration/my-testing/homepage.test.js
+++ cypress/integration/my-testing/homepage.test.js
@@ -1,22 +1,34 @@
 /// <reference types="cypress" />
+import { v4 as uuid } from "uuid";
 
-describe("Homepage", () => {
+describe("Sign Up", () => {
   beforeEach(() => {
     cy.viewport(1440, 900);
-    cy.visit("https://bestbitesuae.dev.engineroomglobal.com/");
+    cy.visit("/signup");
   });
 
-  it("should open homepage", () => {
-    cy.contains("What are you in the mood for?", { timeout: 1 * 8000 }).should(
-      "exist"
-    );
-    cy.contains("Sign up", { timeout: 1 * 8000 }).click();
+  it("should successfully registration", () => {
     cy.url().should("include", "/signup");
+
+    cy.get("#front_name").type("yosa");
+    cy.get("#last_name").type("rama");
+    cy.get("#email").type(`${uuid()}@gmail.com`);
+    cy.get("#password").type("asdf1234");
+    cy.get("#phone_number").type("0238478");
+    cy.get(".comp-defaultButton", { timeout: 1 * 8000 }).click();
+    cy.url({ timeout: 1 * 8000 }).should("include", "/verification");
+    cy.contains("Please verify your account").should("exist");
+  });
+
+  it("should failed registration when user exist", () => {
+    cy.url().should("include", "/signup");
+
     cy.get("#front_name").type("yosa");
     cy.get("#last_name").type("rama");
     cy.get("#email").type("yosarama07@gmail.com");
     cy.get("#password").type("asdf1234");
     cy.get("#phone_number").type("0238478");
-    cy.get(".comp-defaultButton").click();
+    cy.get(".comp-defaultButton", { timeout: 1 * 8000 }).click();
+    cy.contains("Error").should("exist");
   });
 });
